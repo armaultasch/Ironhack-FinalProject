c14a7e24a7fa1f94c2f842fd03abfe69bd09b1c8
Alison Maultasch <armaultasch@gmail.com>
2016-07-26 16:25:08 -0400

    troubleshoot heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  config.cache_classes = false
+  config.log_level = :debug
 
  # config.serve_static_assets = true
 config.assets.compile = true
@@ -14,7 +15,7 @@ config.assets.compile = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
