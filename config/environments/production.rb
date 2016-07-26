95d2a59c452c6e488037fe758f2185fd7f9040a2
Alison Maultasch <armaultasch@gmail.com>
2016-07-26 09:11:15 -0400

    think i foudn it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.serve_static_assets = true
-config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+ config.serve_static_assets = true
 config.assets.compile = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -26,7 +25,7 @@ config.assets.compile = true
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
