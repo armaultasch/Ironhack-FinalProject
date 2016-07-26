49b451b24f5bb6b2c02cf8b75d7579ce0606fadf
Alison Maultasch <armaultasch@gmail.com>
2016-07-26 16:27:23 -0400

    think we got it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = false
-  config.log_level = :debug
+  config.cache_classes = true
 
  # config.serve_static_assets = true
 config.assets.compile = true
@@ -15,7 +14,7 @@ config.assets.compile = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
